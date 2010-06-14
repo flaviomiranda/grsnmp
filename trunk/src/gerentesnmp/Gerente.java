d28d7ebc8b7f8b4044715c7a5a3aaaaf1b12ef20
christianfs <christianfs@b405d5db-4ee8-1011-b25c-2de6feb440f0>
2010-06-14 17:29:04 +0000

    Getbulk implementado.

--- trunk/src/gerentesnmp/Gerente.java
+++ trunk/src/gerentesnmp/Gerente.java
@@ -6,7 +6,7 @@
 package gerentesnmp;
 
 import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 import org.snmp4j.CommunityTarget;
@@ -14,7 +14,7 @@ import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
 import org.snmp4j.Target;
 import org.snmp4j.TransportMapping;
-import org.snmp4j.event.ResponseEvent;
+//import org.snmp4j.event.ResponseEvent;
 import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.Address;
 import org.snmp4j.smi.GenericAddress;
@@ -22,9 +22,9 @@ import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
-import org.snmp4j.util.DefaultPDUFactory;
-import org.snmp4j.util.TableEvent;
-import org.snmp4j.util.TableUtils;
+//import org.snmp4j.util.DefaultPDUFactory;
+//import org.snmp4j.util.TableEvent;
+//import org.snmp4j.util.TableUtils;
 
 /**
  *
@@ -49,8 +49,6 @@ public class Gerente {
         }
     }
 
-    // Since snmp4j relies on asynch req/resp we need a listener
-    // for responses which should be closed
     public void stop() throws IOException {
         snmp.close();
     }
@@ -112,9 +110,6 @@ public class Gerente {
         return target;
     }
 
-    /**
-     * Normally this would return domain objects or something else than this...
-     */
     /*public List<List<String>> getTableAsStrings(OID[] oids) {
         TableUtils tUtils = new TableUtils(snmp, new DefaultPDUFactory());
 
