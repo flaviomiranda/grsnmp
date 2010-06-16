793cad8ba41c21dadd3e5ce06d22a63c1e341a18
christianfs <christianfs@b405d5db-4ee8-1011-b25c-2de6feb440f0>
2010-06-16 05:08:25 +0000

    Threads retiradas por causa da nova implementação.

--- trunk/src/gerentesnmp/Agente.java
+++ trunk/src/gerentesnmp/Agente.java
@@ -13,6 +13,39 @@ public class Agente {
 
     private String ip;
     private String comunidade;
+    private String ifIndex;
+    private String ifDescr;
+    private String ifType;
+    private String ifMtu;
+    private String ifSpeed;
+    private String ifPhysAddress;
+    private String ifAdminStatus;
+    private String ifOperStatus;
+    private String ifLastChange;
+    private String ifInOctets;
+    private String ifInOctetsT2;
+    private String ifInUcastPkts;
+    private String ifInNUcastPkts;
+    private String ifInDiscards;
+    private String ifInErrors;
+    private String ifInUnknownProtos;
+    private String ifOutOctets;
+    private String ifOutOctetsT2;
+    private String ifOutUcastPkts;
+    private String ifOutNUcastPkts;
+    private String ifOutDiscards;
+    private String ifOutErrors;
+    private String ifOutQLen;
+    private String ifSpecific;
+    private String ipInReceives;
+    private String ipInHdrErrors;
+    private String ipInAddrErrors;
+    private String ipForwDatagrams;
+    private String ipForwDatagramsT2;
+    private String ipInUnknownProtos;
+    private String ipInDiscards;
+    private String sysDescr;
+    private String sysUpTime;
 
     public Agente(String ip, String comunidade) {
         this.ip = ip;
@@ -33,5 +66,295 @@ public class Agente {
 
     public void setIp(String ip) {
         this.ip = ip;
+    }
+
+    public String getIfAdminStatus() {
+        return ifAdminStatus;
+    }
+
+    public void setIfAdminStatus(String ifAdminStatus) {
+        this.ifAdminStatus = ifAdminStatus;
+    }
+
+    public String getIfDescr() {
+        return ifDescr;
+    }
+
+    public void setIfDescr(String ifDescr) {
+        this.ifDescr = ifDescr;
+    }
+
+    public String getIfInDiscards() {
+        return ifInDiscards;
+    }
+
+    public void setIfInDiscards(String ifInDiscards) {
+        this.ifInDiscards = ifInDiscards;
+    }
+
+    public String getIfInErrors() {
+        return ifInErrors;
+    }
+
+    public void setIfInErrors(String ifInErrors) {
+        this.ifInErrors = ifInErrors;
+    }
+
+    public String getIfInNUcastPkts() {
+        return ifInNUcastPkts;
+    }
+
+    public void setIfInNUcastPkts(String ifInNUcastPkts) {
+        this.ifInNUcastPkts = ifInNUcastPkts;
+    }
+
+    public String getIfInOctets() {
+        return ifInOctets;
+    }
+
+    public void setIfInOctets(String ifInOctets) {
+        this.ifInOctets = ifInOctets;
+    }
+
+    public String getIfInUcastPkts() {
+        return ifInUcastPkts;
+    }
+
+    public void setIfInUcastPkts(String ifInUcastPkts) {
+        this.ifInUcastPkts = ifInUcastPkts;
+    }
+
+    public String getIfInUnknownProtos() {
+        return ifInUnknownProtos;
+    }
+
+    public void setIfInUnknownProtos(String ifInUnknownProtos) {
+        this.ifInUnknownProtos = ifInUnknownProtos;
+    }
+
+    public String getIfIndex() {
+        return ifIndex;
+    }
+
+    public void setIfIndex(String ifIndex) {
+        this.ifIndex = ifIndex;
+    }
+
+    public String getIfLastChange() {
+        return ifLastChange;
+    }
+
+    public void setIfLastChange(String ifLastChange) {
+        this.ifLastChange = ifLastChange;
+    }
+
+    public String getIfMtu() {
+        return ifMtu;
+    }
+
+    public void setIfMtu(String ifMtu) {
+        this.ifMtu = ifMtu;
+    }
+
+    public String getIfOperStatus() {
+        return ifOperStatus;
+    }
+
+    public void setIfOperStatus(String ifOperStatus) {
+        this.ifOperStatus = ifOperStatus;
+    }
+
+    public String getIfOutDiscards() {
+        return ifOutDiscards;
+    }
+
+    public void setIfOutDiscards(String ifOutDiscards) {
+        this.ifOutDiscards = ifOutDiscards;
+    }
+
+    public String getIfOutErrors() {
+        return ifOutErrors;
+    }
+
+    public void setIfOutErrors(String ifOutErrors) {
+        this.ifOutErrors = ifOutErrors;
+    }
+
+    public String getIfOutNUcastPkts() {
+        return ifOutNUcastPkts;
+    }
+
+    public void setIfOutNUcastPkts(String ifOutNUcastPkts) {
+        this.ifOutNUcastPkts = ifOutNUcastPkts;
+    }
+
+    public String getIfOutOctets() {
+        return ifOutOctets;
+    }
+
+    public void setIfOutOctets(String ifOutOctets) {
+        this.ifOutOctets = ifOutOctets;
+    }
+
+    public String getIfOutQLen() {
+        return ifOutQLen;
+    }
+
+    public void setIfOutQLen(String ifOutQLen) {
+        this.ifOutQLen = ifOutQLen;
+    }
+
+    public String getIfOutUcastPkts() {
+        return ifOutUcastPkts;
+    }
+
+    public void setIfOutUcastPkts(String ifOutUcastPkts) {
+        this.ifOutUcastPkts = ifOutUcastPkts;
+    }
+
+    public String getIfPhysAddress() {
+        return ifPhysAddress;
+    }
+
+    public void setIfPhysAddress(String ifPhysAddress) {
+        this.ifPhysAddress = ifPhysAddress;
+    }
+
+    public String getIfSpecific() {
+        return ifSpecific;
+    }
+
+    public void setIfSpecific(String ifSpecific) {
+        this.ifSpecific = ifSpecific;
+    }
+
+    public String getIfSpeed() {
+        return ifSpeed;
+    }
+
+    public void setIfSpeed(String ifSpeed) {
+        this.ifSpeed = ifSpeed;
+    }
+
+    public String getIfType() {
+        return ifType;
+    }
+
+    public void setIfType(String ifType) {
+        this.ifType = ifType;
+    }
+
+    public String getIfInOctetsT2() {
+        return ifInOctetsT2;
+    }
+
+    public void setIfInOctetsT2(String ifInOctetsT2) {
+        this.ifInOctetsT2 = ifInOctetsT2;
+    }
+
+    public String getIfOutOctetsT2() {
+        return ifOutOctetsT2;
+    }
+
+    public void setIfOutOctetsT2(String ifOutOctetsT2) {
+        this.ifOutOctetsT2 = ifOutOctetsT2;
+    }
+
+    public String getIpForwDatagrams() {
+        return ipForwDatagrams;
+    }
+
+    public void setIpForwDatagrams(String ipForwDatagrams) {
+        this.ipForwDatagrams = ipForwDatagrams;
+    }
+
+    public String getIpForwDatagramsT2() {
+        return ipForwDatagramsT2;
+    }
+
+    public void setIpForwDatagramsT2(String ipForwDatagramsT2) {
+        this.ipForwDatagramsT2 = ipForwDatagramsT2;
+    }
+
+    public String getIpInAddrErrors() {
+        return ipInAddrErrors;
+    }
+
+    public void setIpInAddrErrors(String ipInAddrErrors) {
+        this.ipInAddrErrors = ipInAddrErrors;
+    }
+
+    public String getIpInDiscards() {
+        return ipInDiscards;
+    }
+
+    public void setIpInDiscards(String ipInDiscards) {
+        this.ipInDiscards = ipInDiscards;
+    }
+
+    public String getIpInHdrErrors() {
+        return ipInHdrErrors;
+    }
+
+    public void setIpInHdrErrors(String ipInHdrErrors) {
+        this.ipInHdrErrors = ipInHdrErrors;
+    }
+
+    public String getIpInReceives() {
+        return ipInReceives;
+    }
+
+    public void setIpInReceives(String ipInReceives) {
+        this.ipInReceives = ipInReceives;
+    }
+
+    public String getIpInUnknownProtos() {
+        return ipInUnknownProtos;
+    }
+
+    public void setIpInUnknownProtos(String ipInUnknownProtos) {
+        this.ipInUnknownProtos = ipInUnknownProtos;
+    }
+
+    public String getSysDescr() {
+        return sysDescr;
+    }
+
+    public void setSysDescr(String sysDescr) {
+        this.sysDescr = sysDescr;
+    }
+
+    public String getSysUpTime() {
+        return sysUpTime;
+    }
+
+    public void setSysUpTime(String sysUpTime) {
+        this.sysUpTime = sysUpTime;
+    }
+
+    public void setIfTable(Agente ag){
+        setIfIndex(ag.getIfIndex());
+        setIfDescr(ag.getIfDescr());
+        setIfType(ag.getIfType());
+        setIfMtu(ag.getIfMtu());
+        setIfSpeed(ag.getIfSpeed());
+        setIfPhysAddress(ag.getIfPhysAddress());
+        setIfAdminStatus(ag.getIfAdminStatus());
+        setIfOperStatus(ag.getIfOperStatus());
+        setIfLastChange(ag.getIfLastChange());
+        setIfInOctets(ag.getIfInOctets());
+        setIfInUcastPkts(ag.getIfInUcastPkts());
+        setIfInNUcastPkts(ag.getIfInNUcastPkts());
+        setIfInDiscards(ag.getIfInDiscards());
+        setIfInErrors(ag.getIfInErrors());
+        setIfInUnknownProtos(ag.getIfInUnknownProtos());
+        setIfOutOctets(ag.getIfOutOctets());
+        setIfOutUcastPkts((ag.getIfOutUcastPkts()));
+        setIfOutNUcastPkts((ag.getIfOutNUcastPkts()));
+        setIfOutDiscards(ag.getIfOutDiscards());
+        setIfDescr(ag.getIfDescr());
+        setIfOutErrors(ag.getIfOutErrors());
+        setIfOutQLen(ag.getIfOutQLen());
+        setIfSpecific(ag.getIfSpecific());
     }
 }
--- trunk/src/gerentesnmp/Gerenciador.java
+++ trunk/src/gerentesnmp/Gerenciador.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gerentesnmp;
 
 import java.io.IOException;
@@ -15,49 +14,193 @@ import org.snmp4j.smi.VariableBinding;
  *
  * @author christianfs
  */
-public class Gerenciador implements Runnable {
-    
+public class Gerenciador /*implements Runnable*/ {
+
     private Agente agente;
     private VariableBinding[] respostaGet;
-    private VariableBinding[] respostaGetBulk;
+    private VariableBinding[] respostaGetBulkIfTable;
+    static final String nroEthernet = "2";
 
     public Gerenciador(Agente agente) {
         this.agente = agente;
     }
 
-    public void run() {
-        gerenciar();
-    }
+//    public void run() {
+//        gerenciar();
+//    }
 
-    public void gerenciar(){
+    public Agente gerenciar() {
         List<OID> oidsGet = new ArrayList<OID>();
         OID sysDescr = new OID("1.3.6.1.2.1.1.1.0");
         OID sysUpTime = new OID("1.3.6.1.2.1.1.3.0");
+        OID ipInReceives = new OID("1.3.6.1.2.1.4.3.0");
+        OID ipInHdrErrors = new OID("1.3.6.1.2.1.4.4.0");
+        OID ipInAddrErrors = new OID("1.3.6.1.2.1.4.5.0");
+        OID ipForwDatagrams = new OID("1.3.6.1.2.1.4.6.0");
+        OID ipInUnknownProtos = new OID("1.3.6.1.2.1.4.7.0");
+        OID ipInDiscards = new OID("1.3.6.1.2.1.4.8.0");
         oidsGet.add(sysDescr);
         oidsGet.add(sysUpTime);
+        oidsGet.add(ipInReceives);
+        oidsGet.add(ipInHdrErrors);
+        oidsGet.add(ipInAddrErrors);
+        oidsGet.add(ipForwDatagrams);
+        oidsGet.add(ipInUnknownProtos);
+        oidsGet.add(ipInDiscards);
 
-        List<OID> oidsGetBulk = new ArrayList<OID>();
+        List<OID> oidsGetBulkIfTable = new ArrayList<OID>();
         OID ifTable = new OID("1.3.6.1.2.1.2.2");
-        oidsGetBulk.add(ifTable);
+        oidsGetBulkIfTable.add(ifTable);
 
         Gerente ger = new Gerente(this.agente);
-        try{
+        try {
             respostaGet = ger.get(oidsGet);
             Monitor mon = new Monitor(respostaGet[1].toString());
-            if(mon.monitorar())
-            {
-                for(VariableBinding var : respostaGet)
+            if (mon.monitorar()) {
+                for (VariableBinding var : respostaGet) {
+                    System.out.println(var.toString());
+                    //TODO: SWITCH CASE
+                }
+
+                respostaGetBulkIfTable = ger.getBulk(oidsGetBulkIfTable, 0, 44);
+                for (VariableBinding var : respostaGetBulkIfTable) {
                     System.out.println(var.toString());
 
-                respostaGetBulk = ger.getBulk(oidsGetBulk, 0, 44);
-                for(VariableBinding var : respostaGetBulk)
-                    System.out.println(var.toString());
+                    switch (Integer.parseInt(var.toString().substring(18, 19))) {
+                        case 1:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfIndex((var.toString()).substring(23));
+                            }
+                            break;
+                        case 2:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfDescr((var.toString()).substring(23));
+                            }
+                            break;
+                        case 3:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfType((var.toString()).substring(23));
+                            }
+                            break;
+                        case 4:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfMtu((var.toString()).substring(23));
+                            }
+                            break;
+                        case 5:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfSpeed((var.toString()).substring(23));
+                            }
+                            break;
+                        case 6:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfPhysAddress((var.toString()).substring(23));
+                            }
+                            break;
+                        case 7:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfAdminStatus((var.toString()).substring(23));
+                            }
+                            break;
+                        case 8:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOperStatus((var.toString()).substring(23));
+                            }
+                            break;
+                        case 9:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfLastChange((var.toString()).substring(23));
+                            }
+                            break;
+                        case 10:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInOctets((var.toString()).substring(23));
+                            }
+                            break;
+                        case 11:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInUcastPkts((var.toString()).substring(23));
+                            }
+                            break;
+                        case 12:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInNUcastPkts((var.toString()).substring(23));
+                            }
+                            break;
+                        case 13:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInDiscards((var.toString()).substring(23));
+                            }
+                            break;
+                        case 14:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInErrors((var.toString()).substring(23));
+                            }
+                            break;
+                        case 15:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfInUnknownProtos((var.toString()).substring(23));
+                            }
+                            break;
+                        case 16:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutOctets((var.toString()).substring(23));
+                            }
+                            break;
+                        case 17:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutUcastPkts((var.toString()).substring(23));
+                            }
+                            break;
+                        case 18:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutNUcastPkts((var.toString()).substring(23));
+                            }
+                            break;
+                        case 19:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutDiscards((var.toString()).substring(23));
+                            }
+                            break;
+                        case 20:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutErrors((var.toString()).substring(23));
+                            }
+                            break;
+                        case 21:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfOutQLen((var.toString()).substring(23));
+                            }
+                            break;
+                        case 22:
+                            if (var.toString().substring(20, 21).equals(nroEthernet)) {
+                                this.agente.setIfSpecific((var.toString()).substring(23));
+                            }
+                            break;
+                    }
+                }
             }
             ger.stop();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        return this.agente;
     }
+    //metodo porcentagem de pacotes recebidos com erro
+    /*ifInErrors/(ifInUcastPkts + ifInNUCastPkts + ifInErrors + ifInDiscards +
+    ifInUnknownProtos)**/
+    //metodos taxa de bytes/por segundos
+    /*[(ifInOctets_t2 - ifInOctets_t1) + (ifOutOctets_t2 - ifOutOctets_t1)] /
+    (t2 – t1)**/
+    //metodo utilizacao do link
+    /*[(ifInOctets_t2 - ifInOctets_t1) + (ifOutOctets_t2 - ifOutOctets_t1)] *
+    8 / ifSpeed * (t2-t1)**/
+    //porcentagem  de datagramas recebidos com erro
+    /*
+    (ipInDiscards + ipInHdrErrors + ipInAddrErrors + ipInUnknownProtos) /
+    ipInReceives**/
+    //taxa de  forwarding/segundo
+    /*(ipForwDatagrams_t2 - ipForwDatagrams_t1) / (t2 - t1)**/
+    //metodo  que verifica o SysUptime  dos objetos "deve estar dentro de uma Thread" gera um alarm
 }
